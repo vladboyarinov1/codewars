66b7923c873964b22ef8ff2727fc026135fdd14a
vladboyarinov1 <97247234+vladboyarinov1@users.noreply.github.com>
2023-01-16 09:54:09 +0300

    update codewars

--- 6kyu/SortTheOdd.js
+++ 6kyu/SortTheOdd.js
@@ -1,1 +1,15 @@
+// Task
+//
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
 
+//My solution
+
+    function sortArray(array) {
+    // Return a sorted array.
+    let newArr = [];
+    array
+        .filter((v, i) => v % 2 && newArr.push(i))
+        .sort((a, b) => a - b)
+        .forEach((v, i) => array[newArr[i]] = v);
+    return array
+}
